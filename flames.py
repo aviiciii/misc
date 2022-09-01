956c018f8b763ccbc16741b65ce0406b399d60af
aviiciii <laavesh1@gmail.com>
2022-09-01 17:56:53 +0530

    input error managed

--- flames.py
+++ flames.py
@@ -1,30 +1,39 @@
 
 
 def main():
-    person1, person2 = get_names()
+    while True:
+        try:
+            person1, person2 = get_names()
+            break
+        except:
+            print('Names not valid.')
+
+        
     for letter in person1:
         if letter in person2:
             person2 = person2.replace(letter, '', 1)
             person1 = person1.replace(letter, '', 1)
     
     count = len(person1) + len(person2)
-    print(flames(count))
-
+    res = flames(count)
+    print(res)
+    
 
 def flames(count):
     l = ['F', 'L', 'A', 'M', 'E', 'S']
+    print('---',*l, '---')
     while len(l) != 1:
-        print(*l)
         canceller =  count % len(l)
         l.pop(canceller)
-    print(*l)
     return l[0]
 
 def get_names():
-    p1 = input('P1 Name: ')
-    p2 = input('P2 Name: ')
+    p1 = input('P1 Name: ').strip().replace(' ','')
+    p2 = input('P2 Name: ').strip().replace(' ','')
+    print(p1, p2)
     if p1.isalpha() and p2.isalpha():
-        return [p1.lower().strip(), p2.lower().strip()]
+        return [p1.lower(), p2.lower()]
+    
 
 if __name__ == "__main__":
     main()
