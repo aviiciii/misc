7cb3c22de71d4dbc67bda02d77d22bc8a1ba58f9
aviiciii <laavesh1@gmail.com>
2022-09-01 17:02:14 +0530

    input added

--- flames.py
+++ flames.py
@@ -1,5 +1,14 @@
+
+
 def main():
-    ...
+    person1, person2 = get_names()
+    print(person1, person2)
+
+
+def get_names():
+    p1 = input('P1 Name: ')
+    p2 = input('P2 Name: ')
+    return [p1, p2]
 
 if __name__ == "__main__":
     main()
