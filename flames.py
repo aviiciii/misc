16ee76d73b1c10499cdd3d6d05e671d3bbde6c93
aviiciii <laavesh1@gmail.com>
2022-09-01 18:07:35 +0530

    reorganised into flames function

--- flames.py
+++ flames.py
@@ -1,21 +1,4 @@
-def main():
-    while True:
-        try:
-            person1, person2 = get_names()
-            break
-        except:
-            print('Names not valid.')
-
-        
-    for letter in person1:
-        if letter in person2:
-            person2 = person2.replace(letter, '', 1)
-            person1 = person1.replace(letter, '', 1)
-    
-    count = len(person1) + len(person2)
-    res = flames(count)
-
-    results = {
+results = {
         'F': 'Friends',
         'L': 'Lovers',
         'A': 'Affectionate',
@@ -24,16 +7,44 @@ def main():
         'S': 'Siblings'
     }
 
+def main():
+
+    # getting input names
+    while True:
+        try:
+            p1, p2 = get_names()
+            break
+        except:
+            print('Names not valid.')
+
+    # getting result
+    res = flames(p1, p2)
+
+    
+    # print result
+    print('--- FLAMES ---')
     print(res, ':', results[res])
     
 
-def flames(count):
+def flames(person1, person2):
+
+    # removing common letters
+    for letter in person1:
+        if letter in person2:
+            person2 = person2.replace(letter, '', 1)
+            person1 = person1.replace(letter, '', 1)
+    
+    # getting count of unique letters
+    count = len(person1) + len(person2)
+
+    # iterating over FLAMES
     l = ['F', 'L', 'A', 'M', 'E', 'S']
-    print('---',*l, '---')
+
     while len(l) != 1:
         canceller =  count % len(l)
         l.pop(canceller)
     return l[0]
+
 
 def get_names():
     p1 = input('P1 Name: ').strip().replace(' ','')
