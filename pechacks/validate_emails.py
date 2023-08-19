768c701d3ba256ad4b39ee6795767d70691729dd
aviiciii <laavesh1@gmail.com>
2023-08-19 15:20:12 +0530

    added progress bar

--- pechacks/validate_emails.py
+++ pechacks/validate_emails.py
@@ -1,28 +1,27 @@
-import re
 import smtplib
 import dns.resolver
 import socket
-import sys
+from tqdm import tqdm
 
 
 def validate(email_address):
 	# https://stackoverflow.com/questions/22233848/how-to-verify-an-email-address-in-python-using-smtplib
-	
-	#Step 1: Check email
-	#Check using Regex that an email meets minimum requirements, throw an error if not
+
+	# Step 1: Check email
+	# Check using Regex that an email meets minimum requirements, throw an error if not
 	addressToVerify = email_address
 
-	#Step 2: Getting MX record
-	#Pull domain name from email address
-	domain_name = email_address.split('@')[1]
+	# Step 2: Getting MX record
+	# Pull domain name from email address
+	domain_name = email_address.split("@")[1]
 
-	#get the MX record for the domain
-	records = dns.resolver.resolve(domain_name, 'MX')
+	# get the MX record for the domain
+	records = dns.resolver.resolve(domain_name, "MX")
 	mxRecord = records[0].exchange
 	mxRecord = str(mxRecord)
 
-	#Step 3: ping email server
-	#check if the email address exists
+	# Step 3: ping email server
+	# check if the email address exists
 
 	# Get local server hostname
 	host = socket.gethostname()
@@ -34,7 +33,7 @@ def validate(email_address):
 	# SMTP Conversation
 	server.connect(mxRecord)
 	server.helo(host)
-	server.mail('me@domain.com')
+	server.mail("me@domain.com")
 	code, message = server.rcpt(str(addressToVerify))
 	server.quit()
 
@@ -47,25 +46,23 @@ def validate(email_address):
 
 # print(validate('sec20cs999@sairamtap.edu.in'))
 
-file_name = 'emails.txt'
-with open(file_name, 'r') as f:
+file_name = "emails.txt"
+with open(file_name, "r") as f:
 	emails = f.readlines()
 
 	valid = 0
 	invalid = 0
-	for email in emails:
+	for email in tqdm(emails):
 		email = email.strip()
-		
-		
+
 		if validate(email):
-			with open('valid_emails.txt', 'a') as f:
-				f.write(email + '\n')
+			with open("valid_emails.txt", "a") as f:
+				f.write(email + "\n")
 			valid += 1
 		else:
 			invalid += 1
 
-		print(f'done {valid+invalid} emails')
 
-print('done')
-print(f'valid: {valid}')
-print(f'invalid: {invalid}')
+print("done")
+print(f"valid: {valid}")
+print(f"invalid: {invalid}")
